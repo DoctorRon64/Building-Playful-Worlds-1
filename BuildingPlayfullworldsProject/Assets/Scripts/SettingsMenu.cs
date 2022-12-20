50af32b1aa1a9af72da5c10873179f8cce4dd078
@DoctorRon64 <allesman64@gmail.com>
2022-12-20 18:40:17 +0100

    optionsmenu done

--- BuildingPlayfullworldsProject/Assets/Scripts/SettingsMenu.cs
+++ BuildingPlayfullworldsProject/Assets/Scripts/SettingsMenu.cs
@@ -2,16 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
 	public AudioMixer AudioMixer;
+	public TMP_Dropdown DropdownResolutions;
 	private Resolution[] resolutions;
-
 
 	private void Start()
 	{
 		resolutions = Screen.resolutions;
+		
+		List<string> resolutionList = new List<string>();
+		int currentResolution = 0;
+
+		for (int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			resolutionList.Add(option);
+			if (resolutions[i].width == Screen.currentResolution.width &&
+				resolutions[i].height == Screen.currentResolution.height)
+			{
+				currentResolution = i;
+			}
+		}
+		DropdownResolutions.AddOptions(resolutionList);
+		DropdownResolutions.value = currentResolution;
+		DropdownResolutions.RefreshShownValue();
+	}
+
+	public void SetResolution(int ResolutionIndex)
+	{
+		Resolution resolution = resolutions[ResolutionIndex];
+		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 	}
 
 	public void ChangeVolume(float _Volume)
