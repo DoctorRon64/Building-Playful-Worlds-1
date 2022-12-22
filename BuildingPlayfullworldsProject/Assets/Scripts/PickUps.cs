ac71c7b603c598521740f52066fcb46c5694c630
@DoctorRon64 <allesman64@gmail.com>
2022-12-22 16:26:10 +0100

    piickup trying to fix

--- BuildingPlayfullworldsProject/Assets/Scripts/PickUps.cs
+++ BuildingPlayfullworldsProject/Assets/Scripts/PickUps.cs
@@ -1,26 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PickUps : MonoBehaviour
 {
     public int PickUpType;
-    public int HealthAmount;
     public bool Collided;
-    public PlayerMovement PlayerScript;
 
-	private void OnTriggerEnter(Collider other)
+	public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (PickUpType == 1)
-			{
-                PlayerScript.Health += HealthAmount;
+            Debug.Log("yobama");
+            ChechWichPickUpState();
+        }
+    }
 
-            }
+    public virtual void ChechWichPickUpState()
+	{
+        if (PickUpType == 0)
+		{
+            //do nothing
+            Debug.Log("do notthing");
+            Collided = true;
+            gameObject.SetActive(false);
+        }
+	}
+}
 
+public class HealthPickUps : PickUps
+{
+    public int HealthAmount = 10;
+    public PlayerMovement PlayerScript;
+
+	public override void ChechWichPickUpState()
+	{
+        Debug.Log("joebiden");
+        base.ChechWichPickUpState();
+        Debug.Log("joebiden");
+        if (PickUpType == 1)
+		{
+            PlayerScript.Health += HealthAmount;
+            Collided = true;
+            gameObject.SetActive(false);
+        }
+	}
+}
+
+public class LevelFinishPickup : PickUps
+{
+    public GameObject WinScreen;
+
+	private void Start()
+	{
+		WinScreen.SetActive(false);
+	}
+
+    public override void ChechWichPickUpState()
+    {
+        Debug.Log("joebiden2");
+        base.ChechWichPickUpState();
+        Debug.Log("joebiden2");
+        if (PickUpType == 2)
+        {
+		    WinScreen.SetActive(true);
+            Invoke("ToNextScene", 5);
             Collided = true;
             gameObject.SetActive(false);
         }
     }
+
+    private void ToNextScene()
+	{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
 }
